15cd31e4de534fa858345a6082eb3c1793e2a1fe
Son Luu <sonlh.cs.uit@gmail.com>
2020-11-18 00:55:47 +0700

    correct ex3 and sample for emoji

--- src/Components/Searchbar.js
+++ src/Components/Searchbar.js
@@ -1,17 +1,7 @@
 import "firebase/auth";
 import '../assets/css/searchbar.css'
 import "firebase/firestore";
-import firebase from 'firebase/app'
-import { firebaseConfig } from '../config/firebase.config'
 import React from 'react'
-// di theo nhom
-// try{
-//     firebase.initializeApp(firebaseConfig);
-// }catch(e){
-//     console.log(e.message)
-// }
-
-// const db = firebase.firestore();
 
 
 export function SearchBar(props) {
