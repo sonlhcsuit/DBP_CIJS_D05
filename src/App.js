1b2506b20a6b5cce5183542a58a4f1536fdf6ccc
Son Luu <sonlh.cs.uit@gmail.com>
2020-12-04 21:25:03 +0700

    card-memory-game

--- src/App.js
+++ src/App.js
@@ -1,9 +1,118 @@
 import { Card } from './Components/Card'
+import { Component } from 'react'
 import './App.css';
+const back = 'https://upload.wikimedia.org/wikipedia/vi/3/3b/Pokemon_Trading_Card_Game_cardback.jpg'
+const finish = 'https://media.istockphoto.com/illustrations/cartoon-finish-sign-illustration-id478290143'
+let pokemon_data = [
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/008.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/008.png',
+]
+pokemon_data = pokemon_data.map(img => {
+  return {
+    front: img,
+    back: back,
+    finish: finish,
+    isUp: false,
+    isFlip: 0,
+    isFinish: false
+  }
+})
+// console.log(pokemon_data)
 
 
-export default function App() {
-  return (
-    <Card />
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    pokemon_data.sort((a, b) => Math.random() - Math.random())
+    this.state = {
+      data: pokemon_data,
+      selected: []
+    }
+    this.select = this.select.bind(this)
+    this.flip = this.flip.bind(this)
+    this.check = this.check.bind(this)
+  }
+  check() {
+    let data = JSON.parse(JSON.stringify(this.state.data))
+    let selected = JSON.parse(JSON.stringify(this.state.selected))
+    let a = selected[0]
+    let b = selected[1]
+    // console.log(data[a],data[b])
+    setTimeout(() => {
+      if (data[a].front === data[b].front) {
+        data[a].isFinish = true
+        data[b].isFinish = true
+      } else {
+        data[a].isUp = false
+        data[b].isUp = false
+      }
+      this.setState({
+        data: data,
+        selected: []
+      })
+    }, 250)
+  }
+  componentDidUpdate() {
+    if (this.state.selected.length == 2) {
+      this.check()
+    }
+  }
+
+  select(indx) {
+    this.setState(oldState => {
+      let data = JSON.parse(JSON.stringify(oldState.data))
+      let selected = JSON.parse(JSON.stringify(oldState.selected))
+      if (!data[indx].isFinish) {
+        if (selected.indexOf(indx) == -1) {
+          selected.push(indx)
+        } else {
+          selected = []
+        }
+        data[indx].isUp = !data[indx].isUp
+
+        return {
+          ...oldState,
+          selected,
+          data
+        }
+      }
+    })
+  }
+  flip(indx, val) {
+    // console.log(`flip ${indx} ${val}`)
+    this.setState(oldState => {
+      let data = JSON.parse(JSON.stringify(oldState.data))
+      data[indx].isFlip = val
+      return {
+        data: data
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className="app-cont ">
+        {
+          this.state.data.map((link, indx) => {
+            return <Card {...link} indx={indx} key={indx} select={this.select} flip={this.flip} />
+          })
+        }
+      </div>
+    );
+  }
 }
+export default App
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,29 +1,18 @@
 import React from 'react'
 import '../assets/css/Card.css'
-const back = 'https://upload.wikimedia.org/wikipedia/vi/3/3b/Pokemon_Trading_Card_Game_cardback.jpg'
-const front = 'https://upload.wikimedia.org/wikipedia/vi/thumb/f/f7/Sugimoris025.png/220px-Sugimoris025.png'
-export class Card extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            isFront: 0
+// const front = 'https://upload.wikimedia.org/wikipedia/vi/thumb/f/f7/Sugimoris025.png/220px-Sugimoris025.png'
+export function Card(props) {
 
-        }
-        this.change_face = this.change_face.bind(this)
-    }
-    change_face(val) {
-        console.log('uyep')
-        this.setState({ isFront: val })
-    }
+    return (
+        <div className="card-area " onClick={() => props.flip(props.indx, 1)}>
+            <img className="card-image"
+                src={props.isFinish ? props.finish : props.isUp ? props.front : props.back}
+                flip={props.isFlip}
+                onAnimationEnd={() => { props.flip(props.indx, 0); props.select(props.indx) }}
+                alt="pikachu" />
 
-    render() {
-        console.log(this.state.isFront)
-        return (
-            <div className="card-area" onClick={()=>this.change_face(1)}>
-                <img className="card-image" src={back} flip={this.state.isFront} onAnimationEnd={() => this.change_face(0)} />
-                <input className="btn" type="button" value="Switch" onClick={() => this.change_face(1)} />
-            </div >
-        )
-    }
+        </div >
+    )
+
 
 }
