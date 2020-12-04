fab37094a78faae2551fe94cbbab8696125cbe63
Son Luu <sonlh.cs.uit@gmail.com>
2020-12-04 11:34:38 +0700

    update flip event

--- src/Components/Bulb.jsx
+++ src/Components/Bulb.jsx
@@ -7,18 +7,27 @@ export class Bulb extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            isOn: true
+            isOn: true,
+            isFlip:0
+
         }
         this.switch_bulb = this.switch_bulb.bind(this)
+        this.apply_flip = this.apply_flip.bind(this)
+    }
+    apply_flip(val) {
+        this.setState({ isFlip: val })
     }
     switch_bulb() {
-        this.setState({isOn:!this.state.isOn})
+        this.setState({ isOn: !this.state.isOn })
     }
     render() {
         return (
             <div className="bulb-area">
-                <img className="bulb-image" src={this.state.isOn ? on_img : off_img} />
-                <input className="btn" type="button" value="Switch" onClick={this.switch_bulb} />
+        {/* this.switch_bulb() */}
+                <img className="bulb-image" src={this.state.isOn ? on_img : off_img} flip={this.state.isFlip} onAnimationEnd={() => {this.apply_flip(0);this.switch_bulb()}} />
+                {/* <img className="bulb-image" src={this.state.isOn ? on_img : off_img} /> */}
+
+                <input className="btn" type="button" value="Switch" onClick={()=>this.apply_flip(1)} />
             </div >
         )
     }
