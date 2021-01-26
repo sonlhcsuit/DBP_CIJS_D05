cdf759c87f028b618801b4d3af0a36f957f4f458
Son Luu <sonlh.cs.uit@gmail.com>
2021-01-27 01:06:00 +0700

    fix about size

--- src/Components/BookList.jsx
+++ src/Components/BookList.jsx
@@ -3,13 +3,16 @@ import { Book } from './Book'
 
 export function BookList(props) {
     return (
-        <div className="booklist-area border">
-            {props.books.map(book => {
-                return (
-                    <Book {...book}/>
+        <div className="booklist-area">
+            {
+                props.books.slice(0,10).map(book => {
+                    return (
+                        <Book {...book}/>
+                    )
+                })
 
-                )
-            })}
+            }
         </div>
     )
 }
+
